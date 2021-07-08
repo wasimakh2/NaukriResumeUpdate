f24962b01d6dfc8ffcc956df6cfdd22d7b840464
Wasim Akhtar <Wasim.Akhtar@concentrix.com>
2021-07-08 18:51:19 +0530

    Updates

--- SeleniumHelper/WebDriverAutomation.cs
+++ SeleniumHelper/WebDriverAutomation.cs
@@ -6,7 +6,7 @@ using System.Text;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
 
-namespace BusinessLogic
+namespace SeleniumHelper
 {
     public class WebDriverAutomation
     {
