f01c14f5241908846cc3ef562dc113f3421d5049
Wasim Akhtar <wasim.akh2@gmail.com>
2021-09-18 20:35:06 +0530

    Code Maid Cleaning

--- NaukriResumeUpdate/Program.cs
+++ NaukriResumeUpdate/Program.cs
@@ -1,27 +1,20 @@
 ﻿using System;
 using System.Configuration;
-using Topshelf;
 
 namespace NaukriResumeUpdate
 {
     public class Program
     {
-
-        
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-
             try
             {
-
                 string totalpagetoscrap = ConfigurationManager.AppSettings["totalpagetoscrap"];
 
-
-                for (int i = 1; i < Convert.ToInt32(totalpagetoscrap) ; i++)
+                for (int i = 1; i < Convert.ToInt32(totalpagetoscrap); i++)
                 {
                     BusinessLogic.NaukriJobScrapper.ScrapData(i);
                 }
-
 
                 BusinessLogic.Naukri naukri = new BusinessLogic.Naukri();
                 naukri.UpdateProfile();
@@ -29,18 +22,15 @@ namespace NaukriResumeUpdate
                 naukri.ApplyForJobs();
                 naukri.TearDown();
 
-
                 //var exitCode = HostFactory.Run(x =>
                 //  {
                 //      x.Service<TaskManager>(s =>
                 //      {
-
                 //          s.ConstructUsing(taskmanager => new TaskManager());
 
                 //          s.WhenStarted(taskmanager => taskmanager.Start());
                 //          s.WhenStopped(taskmanager => taskmanager.Stop());
                 //      });
-
 
                 //      x.RunAsLocalSystem();
                 //      x.SetServiceName("NaukriAutomation");
@@ -48,23 +38,17 @@ namespace NaukriResumeUpdate
                 //      x.SetDescription("Daily update Naukri Profile");
                 //  });
 
-
                 //int exitcodevalue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
-
 
                 //Environment.ExitCode = exitcodevalue;
             }
             catch (Exception ex)
             {
-
                 Console.WriteLine(ex.Message);
                 Console.ReadLine();
             }
             Console.WriteLine("Process Completed");
             Console.ReadLine();
-
-
-
         }
     }
 }
